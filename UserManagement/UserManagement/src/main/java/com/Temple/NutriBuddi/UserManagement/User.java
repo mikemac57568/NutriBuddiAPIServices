2b5573962930e56569db94710205dcde6b956057
mikemac57568 <tuf57568@temple.edu>
2017-10-11 13:46:59 -0400

    Initial Commit

---
+++ UserManagement/UserManagement/src/main/java/com/Temple/NutriBuddi/UserManagement/User.java
@@ -0,0 +1,105 @@
+package com.Temple.NutriBuddi.UserManagement;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    
+    @Column(unique = true)
+    private String email;
+    
+    private String password;
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    private int height;
+    
+    private int weight;
+    
+    private int age;
+    
+    private int gender;
+    
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
---
+++ UserManagement/UserManagement/src/main/java/com/Temple/NutriBuddi/UserManagement/UserRepository.java
@@ -0,0 +1,15 @@
+package com.Temple.NutriBuddi.UserManagement;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Temple.NutriBuddi.UserManagement.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	
+	User findByEmail(String email);
+	User findById(int id);
+	
+}
