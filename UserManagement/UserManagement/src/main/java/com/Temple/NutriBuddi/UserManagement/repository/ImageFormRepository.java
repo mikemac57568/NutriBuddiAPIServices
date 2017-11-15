87f0cfec881415e34137741284bb309a689b1f28
pchheu <pchheu@gmail.com>
2017-11-15 14:59:14 -0500

    Added ImageFormController class and delete image method

--- UserManagement/UserManagement/src/main/java/com/Temple/NutriBuddi/UserManagement/repository/ImageFormRepository.java
+++ UserManagement/UserManagement/src/main/java/com/Temple/NutriBuddi/UserManagement/repository/ImageFormRepository.java
@@ -2,6 +2,12 @@ package com.Temple.NutriBuddi.UserManagement.repository;
 
 import com.Temple.NutriBuddi.UserManagement.model.ImageForm;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ImageFormRepository extends CrudRepository<ImageForm, Long> {
+
+    ImageForm findById(int id);
+
+    @Transactional
+    long deleteById(int id);
 }
